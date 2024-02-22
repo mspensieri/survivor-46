b3cea254b9b0faffc1d04bf271cb500297802b2a
Mike Spensieri <mikespensieri@smooch.io>
2024-02-22 17:39:43 -0500

    Fix static asset path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   distDir: "docs",
+  assetPrefix: isProd ? "https://mspensieri.github.com/survivor-46" : undefined,
 };
 
 export default nextConfig;
