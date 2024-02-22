d309704a88cd6b211c61ff1b90d20b740d171ada
Mike Spensieri <mikespensieri@smooch.io>
2024-02-22 18:00:19 -0500

    Switch to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
-const isProd = process.env.NODE_ENV === "production";
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 /** @type {import('next').NextConfig} */
-const nextConfig = isProd
+const nextConfig = isGithubActions
   ? {
       output: "export",
       distDir: "docs",
+      assetPrefix: assetPrefix,
+      basePath: basePath,
     }
   : {};
 
